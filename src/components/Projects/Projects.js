8d4a241d7665e2bf5deb23cfbfff25671f85b736
Patrick <pshin0001@gmail.com>
2024-03-26 05:36:46 +0800

    Update Project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,10 +1,8 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
-import leaf from "../../Assets/Projects/leaf.png";
 import prediction from "../../Assets/Projects/stockPrediction.png";
 import foodOrder from "../../Assets/Projects/foodOrder.png";
-import suicide from "../../Assets/Projects/suicide.png";
 import textSentimentKorean from "../../Assets/Projects/textSentimentKorean.png";
 
 function Projects() {
@@ -44,27 +42,6 @@ function Projects() {
               title="Stock-Prediction"
               description="The objective of this project is to forecast the future prices of stocks through the use of Long Short-Term Memory (LSTM), a variant of recurrent neural networks (RNNs) that are adept at handling time series data."
               ghLink="https://github.com/PateShin/stock-prediction"           
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={leaf}
-              isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={suicide}
-              isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
             />
           </Col>
         </Row>
