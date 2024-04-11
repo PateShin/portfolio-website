b920da9e69478fdb0202e15ab1ce0559c584525c
Patrick <pshin0001@gmail.com>
2024-04-11 20:24:56 +0800

    Add Project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
-import prediction from "../../Assets/Projects/stockPrediction.png";
 import foodOrder from "../../Assets/Projects/foodOrder.png";
+import payBack from "../../Assets/Projects/payBack.png";
+import prediction from "../../Assets/Projects/stockPrediction.png";
 import textSentimentKorean from "../../Assets/Projects/textSentimentKorean.png";
 import sentimentAnalysis from "../../Assets/Projects/sentimentAnalysis.png";
 
@@ -16,6 +17,16 @@ function Projects() {
         <p style={{ color: "white" }}>
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+
+                    <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={payBack}
+              title="PayBack"
+              description="PayBack is a comprehensive financial management application designed to streamline and simplify financial tracking between a company manager and its employees."
+              ghLink="https://github.com/AY2324S2-CS2103T-T12-4/tp"
+            />
+          </Col>
+
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={foodOrder}
